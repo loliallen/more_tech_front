788d3d1a434041ea154eb090d183ac6a76c24ef8
loliallen <levaya.pochta.sobaka@gmail.com>
2021-10-08 21:46:47 +0300

    create presetup

---
+++ src/store/interfaces.ts
@@ -0,0 +1,7 @@
+export type AppState = {
+
+}
+
+export interface IReduxStore {
+    app: AppState
+}
---
+++ src/store/reducers/app.reducer.ts
@@ -0,0 +1,7 @@
+import { Action } from 'redux'
+
+const initalStore = {}
+
+export const appReducer = (store = initalStore, action: Action) => {
+    return store
+}
